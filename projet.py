0deff03dbde9e6bfd8beb21cad615f46919880b6
teach-genius <chantryolanda85@gmail.com>
2024-10-18 19:19:30 +0100

    update

--- projet.py
+++ projet.py
@@ -4,7 +4,5 @@ from criteria import PerformanceCriteria,Opentxt
 data = Opentxt("Score_Sys_1.txt")
 
 criter = PerformanceCriteria(data=data)
-criter.displaygraphe(save=True,point=True,cp="red")
+criter.dispOldDET()
 criter.show()
-
-
